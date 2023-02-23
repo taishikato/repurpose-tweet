1ff9b4eca58f115c5ee3514ad36efae6efd206a7
taishikato <taishi.k0903@gmail.com>
2023-02-23 14:56:29 -0800

    use Next.js 13.2 and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,45 @@
+import { Metadata } from "next";
 import "./globals.css";
+
+const title = "Repurpose Tweet";
+const description =
+  "Effortlessly generate fresh Twitter content with Tweet Repurpose that repurposes your old tweets.";
+const image =
+  "https://hndfjivgskjpoyxfgmyu.supabase.co/storage/v1/object/public/public/ogp";
+const url = "https://repurpose-tweet.vercel.app";
+
+export const metadata: Metadata = {
+  title,
+  description,
+  viewport: "width=device-width, initial-scale=1",
+  authors: [
+    { name: "Taishi" },
+    { name: "Taishi", url: "https://twitter.com/taishik_" },
+  ],
+  publisher: "Taishi",
+  alternates: {
+    canonical: url,
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    title,
+    description,
+    url,
+    siteName: title,
+    type: "website",
+    locale: "en_US",
+    images: image,
+  },
+  twitter: {
+    card: "summary_large_image",
+    title,
+    description,
+    creator: "@taishik_",
+    images: [image],
+  },
+};
 
 export default function RootLayout({
   children,
