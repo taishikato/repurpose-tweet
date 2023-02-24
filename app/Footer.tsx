269f0af3130ab0648333a649ee7840da118a6200
taishikato <taishi.k0903@gmail.com>
2023-02-23 21:26:32 -0800

    add gh link

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,21 +1,34 @@
 import { poppins } from "@/fonts";
+import { IoLogoGithub } from "react-icons/io5";
 
 export const Footer = () => {
   return (
     <footer
-      className={`flex items-center justify-center py-8 border-t ${poppins.className}`}
+      className={`flex justify-center py-8 border-t ${poppins.className}`}
     >
-      <small className="text-xs text-slate-500">
-        Made by{" "}
+      <div className="flex items-center">
+        <small className="text-xs text-slate-500">
+          Made by{" "}
+          <a
+            href="https://twitter.com/taishik_"
+            target="_blank"
+            rel="noreferrer"
+            className="hover:underline"
+          >
+            @taishik_
+          </a>
+        </small>
+        {/* <div className="flex items-center ml-5 text-xs text-slate-500"> */}
         <a
-          href="https://twitter.com/taishik_"
+          href="https://github.com/taishikato/repurpose-tweet"
           target="_blank"
-          rel="noreferrer"
-          className="hover:underline"
+          className="flex items-center text-xs ml-7 text-slate-500 hover:underline"
         >
-          @taishik_
+          <IoLogoGithub className="w-4 h-4 mr-1" />
+          Source code
         </a>
-      </small>
+        {/* </div> */}
+      </div>
     </footer>
   );
 };
